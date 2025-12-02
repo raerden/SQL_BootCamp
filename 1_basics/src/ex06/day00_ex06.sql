883ded8f6867fc002adab8c8b08aaaff1dc882bb
woodsjul <e2e4h@yandex.ru>
2025-12-02 17:43:19 +0700

    Day1 - Basics requests id done

---
+++ 1_basics/src/ex06/day00_ex06.sql
@@ -0,0 +1,13 @@
+SELECT
+    (
+        SELECT p.name
+        FROM person p
+        WHERE p.id = po.person_id) AS name,
+    (
+        SELECT p.name
+        FROM person p
+        WHERE p.id = po.person_id) = 'Denis' AS check_name
+FROM person_order AS po
+WHERE 
+    (po.menu_id = 13 OR po.menu_id = 14 OR po.menu_id = 18)
+    AND po.order_date = '2022-01-07';
